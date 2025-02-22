0e9840ee72f7e7489b3e4c96684e5b1efe7ef54a
gia <giaph92446@gmail.com>
2025-02-22 16:51:14 +0700

    add serveHTTP

--- hello.go
+++ hello.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func Liveness(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+func Readiness(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	http.HandleFunc("/liveness", Liveness)
+	http.HandleFunc("/readiness", Readiness)
+
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		fmt.Println("Error starting server:", err)
+		os.Exit(1)
+	}
 }

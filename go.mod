00c95f0ce37fd3096f3ee091c9119fc3bf85bbd2
gia <giaph92446@gmail.com>
2025-02-23 14:20:14 +0700

    add postgre and redis

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/giagia-gits
 
 go 1.17
+
+require (
+	github.com/go-redis/redis v6.15.9+incompatible // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	github.com/redis/go-redis v6.15.9+incompatible // indirect
+)
--- hello.go
+++ hello.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+
+	"github.com/go-redis/redis"
+	_ "github.com/lib/pq"
 )
 
 func Liveness(w http.ResponseWriter, r *http.Request) {
@@ -17,6 +23,36 @@ func Readiness(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	opt, _ := redis.ParseURL("rediss://default:AUidAAIjcDE0NTg5NTY3MzJiOTg0YTcxOWE2YzI2MDBkYTU5NDQxY3AxMA@stirring-gar-18589.upstash.io:6379")
+	client := redis.NewClient(opt)
+
+	val := client.Get("key1").Val()
+	print(val)
+
+	connStr := "user='koyeb-adm' password=npg_0XkYaq9BCONs host=ep-icy-river-a2npoafz.eu-central-1.pg.koyeb.app dbname='koyebdb'"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+	if _, err := db.ExecContext(context.Background(), "INSERT INTO users (ID, NAME) VALUES ($1, $2)", 1, "user1"); err != nil {
+		log.Fatal(err)
+	}
+	rows, err := db.QueryContext(context.Background(), "SELECT ID, NAME FROM users")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var id int
+		var name string
+		if err := rows.Scan(&id, &name); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("id: %d - name: %s\n", id, name)
+	}
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
@@ -24,6 +60,7 @@ func main() {
 	http.HandleFunc("/liveness", Liveness)
 	http.HandleFunc("/readiness", Readiness)
 
+	fmt.Printf("Server will running on port: %s\n", port)
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		fmt.Println("Error starting server:", err)
 		os.Exit(1)
